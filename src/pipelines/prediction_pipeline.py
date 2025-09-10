d3371616c8efe69839ccb5beab0e1b2a39e341b9
Jhalak Master <jhalakmaster79@gmail.com>
2025-09-11 01:15:17 +0530

    Data Ingestion

--- src/pipelines/prediction_pipeline.py
+++ src/pipelines/prediction_pipeline.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from src.exception import CustomException
-from src.logger import logging
+from src.loger import logging
 from src.utils import load_object
 import pandas as pd
 
